e2769d272a00ff33d2d8d942fea575f876a059f2
rubambig <rubambig@mail.gvsu.edu>
2017-11-12 12:37:40 -0500

    Creates a PCB data structure for a process

---
+++ PCB.java
@@ -0,0 +1,37 @@
+import java.util.*;
+/************************************************************
+ * A Process Control Block (PCB) data structure for a process
+ * @author Gloire Rubambiza
+ * @since 11/12/2017
+ ************************************************************/
+ public class PCB {
+
+   /** The PID for this process. */
+   private int pid;
+
+   /** The page table for this process. */
+   private Hashtable<Integer, Integer> pageTable;
+
+   /** The initial capacity of the page table. */
+   private int capacity = 64;
+
+   /*************************************************
+    * Instantiates a PCB for a process with (minimal)
+    * useful information
+    * The process will have 64 pages of 1KB each
+    * @param pid the process ID
+    * @param table the page table for this process
+    ***********************************************/
+   public PCB (int PID) {
+     this.pid = PID;
+     this.pageTable = new Hashtable<Integer, Integer>(capacity);
+   }
+
+   /***************************************
+    * Update the page table for the process
+    ***************************************/
+    public void updateTable (int page, int frame) {
+      this.pageTable.put(page,frame);
+    }
+
+ }
